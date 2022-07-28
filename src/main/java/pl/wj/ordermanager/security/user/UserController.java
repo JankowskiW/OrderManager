7617943e4204062a30ba2acd95c8956a57aab25a
jankowskiw <waldemar.jankowski95@gmail.com>
2022-07-28 22:26:25 +0200

    Specify privileges for addUser and editUser UserController methods

--- src/main/java/pl/wj/ordermanager/security/user/UserController.java
+++ src/main/java/pl/wj/ordermanager/security/user/UserController.java
@@ -1,6 +1,7 @@
 package pl.wj.ordermanager.security.user;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import pl.wj.ordermanager.security.user.model.dto.UserCredentialsDto;
 import pl.wj.ordermanager.security.user.model.dto.UserRequestDto;
@@ -15,11 +16,13 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/users")
+    @PreAuthorize("hasAuthority('USER_WRITE')")
     public UserResponseDto addUser(@RequestBody UserRequestDto userRequestDto) {
         return userService.addUser(userRequestDto);
     }
 
     @PatchMapping("/users/{id}")
+    @PreAuthorize("hasAuthority('USER_WRITE')")
     public UserResponseDto editUser(
             @PathVariable long id, @Valid @RequestBody UserUpdateRequestDto userUpdateRequestDto) {
             return userService.editUser(userUpdateRequestDto);
