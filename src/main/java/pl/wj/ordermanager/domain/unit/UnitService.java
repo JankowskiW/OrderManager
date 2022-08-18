0ab3c51cf6cc94e0e504a3848b8ba9b74a71fe9c
jankowskiw <waldemar.jankowski95@gmail.com>
2022-08-18 18:11:18 +0200

    Implement addUnit() method from UnitService

--- src/main/java/pl/wj/ordermanager/domain/unit/UnitService.java
+++ src/main/java/pl/wj/ordermanager/domain/unit/UnitService.java
@@ -1,10 +1,12 @@
 package pl.wj.ordermanager.domain.unit;
 
 import lombok.RequiredArgsConstructor;
-import org.hibernate.cfg.NotYetImplementedException;
+import org.mapstruct.factory.Mappers;
 import org.springframework.stereotype.Service;
 import pl.wj.ordermanager.domain.unit.model.Unit;
+import pl.wj.ordermanager.domain.unit.model.UnitMapper;
 import pl.wj.ordermanager.domain.unit.model.dto.UnitRequestDto;
+import pl.wj.ordermanager.exception.ResourceExistsException;
 
 import java.util.List;
 
@@ -13,11 +15,15 @@ import java.util.List;
 public class UnitService {
     private final UnitRepository unitRepository;
 
+    private static UnitMapper unitMapper = Mappers.getMapper(UnitMapper.class);
+
     public List<Unit> getUnits() {
         return unitRepository.findAll();
     }
 
     public Unit addUnit(UnitRequestDto unitRequestDto) {
-        throw new NotYetImplementedException();
+        if(unitRepository.existsByName(unitRequestDto.getName())) throw new ResourceExistsException("Unit", "name");
+        Unit unit = unitMapper.unitRequestDtoToUnit(unitRequestDto);
+        return unitRepository.save(unit);
     }
 }
--- src/main/java/pl/wj/ordermanager/domain/unit/model/UnitMapper.java
+++ src/main/java/pl/wj/ordermanager/domain/unit/model/UnitMapper.java
@@ -1,7 +1,9 @@
 package pl.wj.ordermanager.domain.unit.model;
 
 import org.mapstruct.Mapper;
+import pl.wj.ordermanager.domain.unit.model.dto.UnitRequestDto;
 
 @Mapper(componentModel = "spring")
 public interface UnitMapper {
+    Unit unitRequestDtoToUnit(UnitRequestDto unitRequestDto);
 }
